2711c9848ba6ef8869c75d2a134fa44414ce5573
mshemeel007 <mshemeel007@gmail.com>
2023-01-31 21:08:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/custom/annotation/SpringBootCustomAnnotation.java
@@ -0,0 +1,13 @@
+package com.example.custom.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomAnnotation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomAnnotation.class, args);
+	}
+
+}
